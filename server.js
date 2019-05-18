7ca8d5a157e95d679080dac049f4a1eec5a0377f
Ronny Alvarado <rsalvarado777@gmail.com>
2019-05-18 11:38:54 -0500

    Created endpoint for adding a new project into the database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(logger("dev"));
 const projectdb = require("./data/helpers/projectModel.js");
 const actiondb = require("./data/helpers/actionModel.js");
 
-server.get("/api/projects", async (req, res) => {
+server.get("/api/projects/", async (req, res) => {
   try {
     const getProjects = await projectdb.get();
     res.send(getProjects);
@@ -20,6 +20,44 @@ server.get("/api/projects", async (req, res) => {
     res
       .status(500)
       .json({ message: "There was a problem retrieving all of the projects." });
+  }
+});
+
+server.get("/api/projects/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getProjects = await projectdb.get(id);
+    res.send(getProjects);
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        message:
+          "There was a problem retrieving all of the actions of the specified project."
+      });
+  }
+});
+
+server.post("/api/projects", async (req, res) => {
+  try {
+    const newProject = req.body;
+    const addProject = await projectdb.insert(newProject);
+    if (addProject.name && addProject.description) {
+      res.status(201).json(addProject);
+    } else {
+      res
+        .status(400)
+        .json({
+          message:
+            "A name and description is required. Please enter a name and description."
+        });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        message: "Something went wrong when attempting to add a new project."
+      });
   }
 });
 
