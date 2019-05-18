0b6599e3512b1000b0ed92cb190b1b6621b69218
Ronny Alvarado <rsalvarado777@gmail.com>
2019-05-18 11:45:10 -0500

    Created endpoint for removing a project from the database

--- server.js
+++ server.js
@@ -23,41 +23,54 @@ server.get("/api/projects/", async (req, res) => {
   }
 });
 
-server.get("/api/projects/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const getProjects = await projectdb.get(id);
-    res.send(getProjects);
-  } catch (err) {
-    res
-      .status(500)
-      .json({
-        message:
-          "There was a problem retrieving all of the actions of the specified project."
-      });
-  }
-});
+// server.get("/api/projects/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const getProjects = await projectdb.get(id);
+//     res.send(getProjects);
+//   } catch (err) {
+//     res
+//       .status(500)
+//       .json({
+//         message:
+//           "There was a problem retrieving all of the actions of the specified project."
+//       });
+//   }
+// });
 
 server.post("/api/projects", async (req, res) => {
   try {
     const newProject = req.body;
     const addProject = await projectdb.insert(newProject);
-    if (addProject.name && addProject.description) {
+    const { name, description } = req.body;
+    if (name.length > 0 && description.length > 0) {
       res.status(201).json(addProject);
     } else {
-      res
-        .status(400)
-        .json({
-          message:
-            "A name and description is required. Please enter a name and description."
-        });
+      res.status(400).json({
+        message:
+          "A name and description is required. Please enter a name and description."
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Something went wrong when attempting to add a new project."
+    });
+  }
+});
+
+server.delete("/api/projects/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteProject = await projectdb.remove(id);
+    if (deleteProject) {
+      res.status(200).json(deleteProject);
+    } else {
+      res.status(404).json({ message: "Project id not found." });
     }
   } catch (err) {
     res
       .status(500)
-      .json({
-        message: "Something went wrong when attempting to add a new project."
-      });
+      .json({ message: "There was an error in processing your request." });
   }
 });
 
